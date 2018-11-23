65dad9d45cd744e35f83fa7a0279904232809619
taeeui <xodml78@gmail.com>
2018-11-23 22:13:25 +0900

    태의가올림

--- app/src/main/java/com/medetector/jmt/medetector/MainActivity.java
+++ app/src/main/java/com/medetector/jmt/medetector/MainActivity.java
@@ -2,6 +2,9 @@ package com.medetector.jmt.medetector;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ClickButton1(View v) {
+        Toast.makeText(getApplicationContext(), "이미지를 눌렀습니다.", Toast.LENGTH_LONG).show();
     }
 }
